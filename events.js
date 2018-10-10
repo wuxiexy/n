3aa374ce3f8b6c855deefe5990c2b2c9f9769dce
wuxiexy <wuxiexy@sina.cn>
2018-10-10 18:07:47 +0800

    test

--- events.js
+++ events.js
@@ -13,6 +13,8 @@
 // 这些对象开放了一个 eventEmitter.on() 函数，允许将一个或多个函数绑定到会被对象触发的命名事件上。
 
 
+
+
 // ******************************************************************************************************
 // 当 EventEmitter 对象触发一个事件时，【所有绑定在该事件上的函数都被同步地调用】。 监听器的返回值会被丢弃。
 
@@ -21,8 +23,10 @@
 // ******************************************************************************************************
 
 
+
 const EventEmitter = require('events');
 class MyEmitter extends EventEmitter {}
+
 
 
 // 实例化 事件对象
@@ -40,6 +44,10 @@ myEmitter.on('ev', ()=>{
     console.log('触发了ev事件');
 });
 
+
+// **********************************************************************************************
+// 按照监听器注册的顺序，同步地调用每个注册到名为 eventName 的事件的监听器，并传入提供的参数。
+// 如果事件有监听器，则返回 true，否则返回 false。
 
 // eventEmitter.emit() 方法用于触发事件。
 // myEmitter.emit('ev');
@@ -83,8 +91,8 @@ let m = 0;
 myEmitter.once('event1', () => {
     console.log(++m);
 });
-myEmitter.emit('event1');           // 打印: 1
-myEmitter.emit('event1');           // 忽略
+// myEmitter.emit('event1');           // 打印: 1
+// myEmitter.emit('event1');           // 忽略
 
 
 
@@ -98,29 +106,98 @@ myEmitter.on('error', (err) => {
     console.error('有错误');
 });
 
-myEmitter.emit('error', new Error('whoops!'));
+// myEmitter.emit('error', new Error('whoops!'));
 
 
 
 
 
 
+// 'newListener' 事件
+// EventEmitter 实例在新的监听器被添加到其内部监听器数组之前，会触发自身的 'newListener' 事件。
+// 在添加监听器之前触发 'newListener' 事件有一个副作用： 如果在回调中注册同名事件的监听器，
+// 则该监听器会被插入到正被添加的监听器前面。
+myEmitter.once('newListener', (event, listener) => {
+    if (event === 'event2') {
+        // 在注册监听器前面插入一个新的监听器。
+        myEmitter.on('event2', () => {
+            console.log('这个是添加监听器之前触发 newListener 事件里面注册的监听器 B');
+        });
+    }
+
+    listener();         // 这个是你测试的监听器，event2， 这里相当于 myEmitter.emit('event2');
+});
+/*myEmitter.on('event2', () => {
+    console.log('这个是myEmitter注册的监听器 A');
+});
+myEmitter.emit('event2');
+*/
 
 
 
 
+// 'removeListener' 事件
+function callback(stream) {
+    console.log('someone connected!');
+}
+myEmitter.on('connection', callback);
+// myEmitter.removeListener('connection', callback);
+myEmitter.off('connection', callback);                  // 新增于: v10.0.0
+myEmitter.emit('connection');
 
 
 
 
+// 返回事件句柄的监听数
+// console.log(myEmitter.listeners('event2'));      // 历史版本
+// console.log(myEmitter.listenerCount('event'));      // 新版本
 
 
 
 
+// myEmitter.emit(event, [arg1], [arg2], [...])
+// 按参数的顺序执行每个监听器，如果事件有注册监听返回 true，否则返回 false。
 
 
 
+/*myEmitter.setMaxListeners(myEmitter.getMaxListeners() + 2);
 
+// EventEmitter.defaultMaxListeners
+myEmitter.on('event', () => {
+    console.log('defaultMaxListeners');
+});
+myEmitter.on('event', () => {
+    console.log('defaultMaxListeners');
+});
+myEmitter.on('event', () => {
+    console.log('defaultMaxListeners');
+});
+myEmitter.on('event', () => {
+    console.log('defaultMaxListeners');
+});
+myEmitter.on('event', () => {
+    console.log('defaultMaxListeners');
+});
+myEmitter.on('event', () => {
+    console.log('defaultMaxListeners');
+});
+myEmitter.on('event', () => {
+    console.log('defaultMaxListeners');
+});
+myEmitter.on('event', () => {
+    console.log('defaultMaxListeners');
+});
+myEmitter.on('event', () => {
+    console.log('defaultMaxListeners');
+});
+myEmitter.on('event', () => {
+    console.log('defaultMaxListeners');
+});*/
+
+
+
+// emitter.eventNames()
+// console.log(myEmitter.eventNames());    // 返回已注册监听器的事件名数组
 
 
 
---
+++ fs.js
@@ -0,0 +1,26 @@
+/*
+* fs - 文件系统
+* document【http://nodejs.cn/api/fs.html】
+*
+*
+* */
+const fs = require('fs');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
